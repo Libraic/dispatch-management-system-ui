8039a0a2699b42f75e0a7a99e98ee6ac10128127
Victor Nani <victornani07@gmail.com>
2026-02-13 12:34:57 +0200

    Made the blue color the sytem color

--- src/tailwind/tailwind-colors-vars.ts
+++ src/tailwind/tailwind-colors-vars.ts
@@ -10,21 +10,22 @@
 
 export const BACKGROUND_PALE_BLUE = "bg-pale-blue";
 export const OUTLINE_PALE_BLUE = "outline-pale-blue";
-export const BACKGROUND_NORMAL_COLOR = "bg-normal-purple";
-export const BACKGROUND_SOLID_COLOR = "bg-solid-purple";
+
+export const BACKGROUND_NORMAL_COLOR = "bg-light-blue";
+export const BACKGROUND_SOLID_COLOR = "bg-solid-blue";
 export const BACKGROUND_BLUE_GREY_COLOR = "bg-blue-grey";
-export const BORDER_NORMAL_COLOR = "border-normal-purple";
-export const BORDER_SOLID_COLOR = "border-solid-purple";
+export const BORDER_NORMAL_COLOR = "border-light-blue";
+export const BORDER_SOLID_COLOR = "border-solid-blue";
 export const BORDER_PALE_BLUE = "border-pale-blue";
-export const TEXT_NORMAL_COLOR = "text-normal-purple";
+export const TEXT_NORMAL_COLOR = "text-light-blue";
 export const TEXT_SOLID_GRAY = "text-solid-gray";
 
-export const HOVER_BORDER_NORMAL_COLOR = "hover:border-normal-purple";
-export const HOVER_BORDER_SOLID_COLOR = "hover:border-solid-purple";
-export const HOVER_BORDER_B_NORMAL_COLOR = "hover:border-b-normal-purple";
-export const HOVER_BACKGROUND_NORMAL_COLOR = "hover:bg-normal-purple";
-export const HOVER_BACKGROUND_SOLID_COLOR = "hover:bg-solid-purple";
+export const HOVER_BORDER_NORMAL_COLOR = "hover:border-light-blue";
+export const HOVER_BORDER_SOLID_COLOR = "hover:border-solid-blue";
+export const HOVER_BORDER_B_NORMAL_COLOR = "hover:border-b-light-blue";
+export const HOVER_BACKGROUND_NORMAL_COLOR = "hover:bg-light-blue";
+export const HOVER_BACKGROUND_SOLID_COLOR = "hover:bg-solid-blue";
 export const HOVER_BACKGROUND_PALE_BLUE = "hover:bg-pale-blue";
-export const HOVER_TEXT_SOLID_COLOR = "hover:text-solid-purple";
+export const HOVER_TEXT_SOLID_COLOR = "hover:text-solid-blue";
 
 export const ODD_BACKGROUND_LIGHT_GRAY = "odd:bg-light-gray";
--- src/tailwind/tailwind-font-vars.ts
+++ src/tailwind/tailwind-font-vars.ts
@@ -1,3 +1,9 @@
+/**
+ * The fonts are defined in the index.css file. This file is used to export the fonts,
+ * in different variations, as variables. If a change is made to the fonts in the index.css file,
+ * they will be reflected here. If a change is made to the variables defining the fonts
+ * in the index.css file, these variables will have to be manually updated here.
+ */
 export const SYSTEM_FONT_THIN = "font-inter-tight font-thin";
 export const SYSTEM_FONT_LIGHT = "font-inter-tight font-light";
 export const SYSTEM_FONT_NORMAL = "font-inter-tight font-normal";
