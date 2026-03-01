dd74414e5408cbd661275971885829e39f5ed7d2
Victor Nani <victornani07@gmail.com>
2026-03-01 12:04:19 +0200

    Redesign the Company Sidebar menu

--- src/components/Common/Typography/HoverableDescription.tsx
+++ src/components/Common/Typography/HoverableDescription.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import errorIcon from "../../../assets/global/error.svg";
+import { SYSTEM_FONT_LIGHT } from "../../../tailwind/tailwind-font-vars.ts";
 
 export const HoverableDescription: React.FC<{
   message: string;
@@ -7,11 +8,11 @@ export const HoverableDescription: React.FC<{
 }> = ({ message, icon }) => {
   return (
     <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 z-[10000]">
-      <div className="flex flex-row items-center justify-center w-max bg-gray-100 rounded-xl opacity-90 p-2 shadow-lg backdrop-blur-sm whitespace-nowrap">
+      <div className="flex flex-row items-center justify-center w-fit bg-gray-100 rounded-xl opacity-90 p-2 shadow-lg backdrop-blur-sm whitespace-nowrap">
         {icon && (
           <img src={errorIcon} alt="error-icon" className="w-4 h-4 mr-1" />
         )}
-        <p className="font-lato font-light text-black text-[0.8rem]">
+        <p className={`${SYSTEM_FONT_LIGHT} text-black text-[0.6rem]`}>
           {message}
         </p>
       </div>
--- src/components/Company/Sidebar/public/CompanySidebar.tsx
+++ src/components/Company/Sidebar/public/CompanySidebar.tsx
@@ -1,77 +1,96 @@
-import { CompanySidebarHeader } from "../internal/CompanySidebarHeader.tsx";
-import { BLANK_STRING } from "../../../../constants/common/global-constants.ts";
 import { CompanySidebarItem } from "../internal/CompanySidebarItem.tsx";
 import trucksBoardUnhoveredIcon from "../../../../assets/company-menu/trucks-board-unhovered.svg";
 import {
   DISPATCHERS_VIEW,
   DRIVERS_VIEW,
-  FINANCIAL_REPORTS,
-  LOAD_BY_LOAD_REPORTS,
   TRAILERS_VIEW,
   TRUCKS_BOARD,
   TRUCKS_VIEW,
 } from "../../../../constants/route/internal-route-constants.ts";
+import { useParams } from "react-router-dom";
+import * as React from "react";
+import type { SidebarState } from "../../../../types/internal/sidebar/sidebar-types.ts";
+import { SYSTEM_FONT_LIGHT } from "../../../../tailwind/tailwind-font-vars.ts";
 import addDriverIcon from "../../../../assets/company-menu/add-driver.svg";
 import addAssetIcon from "../../../../assets/company-menu/add-asset-icon.svg";
 import addDispatcherIcon from "../../../../assets/company-menu/add-dispatcher-icon.svg";
-import reportsIcon from "../../../../assets/reports/reports.svg";
-import { useParams } from "react-router-dom";
-import * as React from "react";
-import type { CompanyData } from "../../../../types/api/company/company-api-response-types.ts";
+import sidebarCloserIcon from "../../../../assets/company-menu/sidebar-closer.svg";
+import sidebarOpenerIcon from "../../../../assets/company-menu/sidebar-opener.svg";
+import dashboardIcon from "../../../../assets/company-menu/dashboard.svg";
 
-export const CompanySidebar: React.FC<{ company: CompanyData }> = ({
-  company,
-}) => {
+export const CompanySidebar: React.FC<{
+  sidebarState: SidebarState;
+  setSidebarState: React.Dispatch<React.SetStateAction<SidebarState>>;
+}> = ({ sidebarState, setSidebarState }) => {
   const { companyUuid } = useParams();
   const baseRoute = `/dashboard/${companyUuid}`;
+  const [sidebarActiveIcon, setSidebarActiveIcon] =
+    React.useState(sidebarCloserIcon);
   return (
-    <div className="w-1/10 bg-solid-black py-5">
-      <CompanySidebarHeader
-        companyName={company ? company.name : BLANK_STRING}
+    <div
+      className={`${sidebarState === "open" ? "w-[12rem]" : "w-[4rem]"} py-5 border-r-1 border-gray-200 bg-[#f9f9f9] h-screen`}
+    >
+      <div className="w-full flex items-center justify-between px-[0.7rem]">
+        {sidebarState === "open" && (
+          <p className={`pl-1 ${SYSTEM_FONT_LIGHT} tracking-wide`}>
+            Kovin Group
+          </p>
+        )}
+        <img
+          className="hover:bg-gray-200 rounded-[0.5rem] w-8 h-8"
+          onClick={() => {
+            setSidebarState((prev) => (prev === "open" ? "closed" : "open"));
+            setSidebarActiveIcon((prev) =>
+              prev === sidebarCloserIcon
+                ? sidebarOpenerIcon
+                : sidebarCloserIcon,
+            );
+          }}
+          src={sidebarActiveIcon}
+          alt="img-icon"
+        />
+      </div>
+      <CompanySidebarItem
+        label="Dashboard"
+        icon={dashboardIcon}
+        baseRoute={baseRoute}
+        sidebarState={sidebarState}
       />
       <CompanySidebarItem
         label="Trucks Board"
         icon={trucksBoardUnhoveredIcon}
         baseRoute={`${baseRoute}${TRUCKS_BOARD}`}
+        sidebarState={sidebarState}
       />
       <CompanySidebarItem
         label="Drivers"
         icon={addDriverIcon}
         baseRoute={`${baseRoute}${DRIVERS_VIEW}`}
+        sidebarState={sidebarState}
       />
       <CompanySidebarItem
         label="Dispatchers"
         icon={addDispatcherIcon}
         baseRoute={`${baseRoute}${DISPATCHERS_VIEW}`}
+        sidebarState={sidebarState}
       />
-      <CompanySidebarItem
-        label="Assets"
-        icon={addAssetIcon}
-        submenuData={[
-          {
-            label: "Trucks",
-            route: `${baseRoute}${TRUCKS_VIEW}`,
-          },
-          {
-            label: "Trailers",
-            route: `${baseRoute}${TRAILERS_VIEW}`,
-          },
-        ]}
-      />
-      <CompanySidebarItem
-        label="Reports"
-        icon={reportsIcon}
-        submenuData={[
-          {
-            label: "Financial",
-            route: `${baseRoute}${FINANCIAL_REPORTS}`,
-          },
-          {
-            label: "Load-by-Load",
-            route: `${baseRoute}${LOAD_BY_LOAD_REPORTS}`,
-          },
-        ]}
-      />
+      {sidebarState === "open" && (
+        <CompanySidebarItem
+          label="Assets"
+          icon={addAssetIcon}
+          sidebarState={sidebarState}
+          submenuData={[
+            {
+              label: "Trucks",
+              route: `${baseRoute}${TRUCKS_VIEW}`,
+            },
+            {
+              label: "Trailers",
+              route: `${baseRoute}${TRAILERS_VIEW}`,
+            },
+          ]}
+        />
+      )}
     </div>
   );
 };
