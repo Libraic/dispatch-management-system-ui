83ff1a2163cb4328feeb6f185b72099428b71b72
Victor Nani <victornani07@gmail.com>
2026-04-22 15:26:29 +0300

    Adjust the background/border color for the Submit button

--- src/ui/Buttons/SubmitButton.tsx
+++ src/ui/Buttons/SubmitButton.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import {
   BORDER_SOLID_COLOR,
-  HOVER_BACKGROUND_SOLID_COLOR,
-  HOVER_BORDER_SOLID_COLOR,
+  HOVER_BACKGROUND_NORMAL_COLOR,
+  HOVER_BORDER_NORMAL_COLOR,
 } from "#/shared/constants/tailwind/tailwindColors.constants";
 
 type SubmitButtonProps = {
@@ -19,7 +19,7 @@ export const SubmitButton: React.FC<SubmitButtonProps> = ({
   const interactable = isInteractable ?? true;
   return (
     <button
-      className={`min-w-[5rem] border-1 ${BORDER_SOLID_COLOR} rounded-3xl text-black bg-white  text-[1rem] py-[0.2rem] px-[0.4rem] ${HOVER_BACKGROUND_SOLID_COLOR} ${HOVER_BORDER_SOLID_COLOR} hover:text-white ${interactable ? "hover:cursor-pointer" : "hover:cursor-not-allowed"} font-medium transition-all ease-in duration-150`}
+      className={`min-w-[5rem] border-1 ${BORDER_SOLID_COLOR} rounded-3xl text-black bg-white  text-[1rem] py-[0.2rem] px-[0.4rem] ${HOVER_BACKGROUND_NORMAL_COLOR} ${HOVER_BORDER_NORMAL_COLOR} hover:text-white ${interactable ? "hover:cursor-pointer" : "hover:cursor-not-allowed"} font-medium transition-all ease-in duration-150`}
       onClick={(e: React.FormEvent) => {
         if (interactable) {
           action(e);
